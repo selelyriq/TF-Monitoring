2f3303b09acd8a0c430ce4a9c677ac92786073c0
lyriq-cloudsolutions <lyriq@geniouscloudsolutions.com>
2025-04-04 14:57:10 -0400

    added agent support

--- main.tf
+++ main.tf
@@ -1,26 +1,65 @@
 terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 5.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
+  }
 }
-resource "aws_cloudwatch_metric_filter" "CostAllocationTagFilter" {
-    name = var.name
-    metric_filter = var.metric_filter
-    metric_transformation = var.metric_transformation
+resource "aws_cloudwatch_log_metric_filter" "CostAllocationTagFilter" {
+  name           = var.name
+  pattern        = var.pattern
+  log_group_name = var.log_group_name
+  metric_transformation {
+    name      = var.metric_name
     namespace = var.namespace
-    pattern = var.pattern
+    value     = var.value
+  }
 }
 
 resource "aws_cloudwatch_metric_alarm" "CostAlloactionTagAlarm" {
-    alarm_name = var.alarm_name
-    comparison_operator = var.comparison_operator
-    evaluation_periods = var.evaluation_periods
-    threshold = var.threshold
-    statistic = var.statistic
-    metric_name = var.metric_name
-    namespace = var.namespace
+  alarm_name          = var.alarm_name
+  comparison_operator = var.comparison_operator
+  evaluation_periods  = var.evaluation_periods
+  threshold           = var.threshold
+  statistic           = var.statistic
+  metric_name         = var.metric_name
+  namespace           = var.namespace
+}
+
+########################################################
+# EC2 to access CloudWatch Metrics
+########################################################
+
+resource "aws_iam_role" "ec2_cloudwatch_role" {
+  name = "ec2_cloudwatch_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_cloudwatch_policy_attachment" {
+  role       = aws_iam_role.ec2_cloudwatch_role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+}
+
+resource "aws_iam_instance_profile" "ec2_cloudwatch_profile" {
+  name = var.instance_profile_name
+  role = aws_iam_role.ec2_cloudwatch_role.name
+}
+
+resource "aws_cloudwatch_log_group" "cost_allocation_tag_log_group" {
+  name              = var.log_group_name
+  retention_in_days = var.retention_in_days
+  tags              = var.tags
 }
 
--- variables.tf
+++ variables.tf
@@ -1,54 +1,74 @@
 variable "name" {
-    type = string
-    description = "The name of the metric filter"
+  type        = string
+  description = "The name"
 }
-
-variable "metric_filter" {
-    type = string
-    description = "The metric filter"
-}
-
-variable "metric_transformation" {
-    type = string
-    description = "The metric transformation"
-}
-
-variable "namespace" {
-    type = string
-    description = "The namespace"
-}  
 
 variable "pattern" {
-    type = string
-    description = "The pattern"
-}
-
-variable "alarm_name" {
-    type = string
-    description = "The alarm name"
-}
-
-variable "comparison_operator" {
-    type = string
-    description = "The comparison operator"
-}
-
-variable "evaluation_periods" {
-    type = number
-    description = "The evaluation periods"
-}
-
-variable "threshold" {
-    type = number
-    description = "The threshold"
-}
-
-variable "statistic" {
-    type = string
-    description = "The statistic"
+  type        = string
+  description = "The pattern"
 }
 
 variable "metric_name" {
-    type = string
-    description = "The metric name"
+  type        = string
+  description = "The metric name"
 }
+
+variable "namespace" {
+  type        = string
+  description = "The namespace"
+}
+
+variable "value" {
+  type        = string
+  description = "The value"
+}
+
+variable "alarm_name" {
+  type        = string
+  description = "The alarm name"
+}
+
+variable "comparison_operator" {
+  type        = string
+  description = "The comparison operator"
+}
+
+variable "evaluation_periods" {
+  type        = number
+  description = "The evaluation periods"
+}
+
+variable "threshold" {
+  type        = number
+  description = "The threshold"
+}
+
+variable "statistic" {
+  type        = string
+  description = "The statistic"
+}
+
+variable "instance_profile_name" {
+  type        = string
+  description = "The instance profile name"
+}
+
+variable "log_group_name" {
+  type        = string
+  description = "The log group name"
+}
+
+variable "retention_in_days" {
+  type        = number
+  description = "The retention in days"
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "The tags"
+}
+
+
+
+
+
