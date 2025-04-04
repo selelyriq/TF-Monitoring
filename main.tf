29b084088b51a69884bc754ca1240e3a7d3a5891
lyriq-cloudsolutions <lyriq@geniouscloudsolutions.com>
2025-04-04 15:59:37 -0400

    i should do some swe

--- main.tf
+++ main.tf
@@ -6,15 +6,27 @@ terraform {
     }
   }
 }
+
+# Create the log group first
+resource "aws_cloudwatch_log_group" "cost_allocation_tag_log_group" {
+  name              = var.log_group_name
+  retention_in_days = var.retention_in_days
+  tags              = var.tags
+}
+
+# Then create the metric filter with explicit dependency
 resource "aws_cloudwatch_log_metric_filter" "CostAllocationTagFilter" {
   name           = var.name
   pattern        = var.pattern
-  log_group_name = var.log_group_name
+  log_group_name = aws_cloudwatch_log_group.cost_allocation_tag_log_group.name  # Reference the log group resource
+  
   metric_transformation {
     name      = var.metric_name
     namespace = var.namespace
     value     = var.value
   }
+
+  depends_on = [aws_cloudwatch_log_group.cost_allocation_tag_log_group]  # Explicit dependency
 }
 
 resource "aws_cloudwatch_metric_alarm" "CostAlloactionTagAlarm" {
@@ -22,10 +34,10 @@ resource "aws_cloudwatch_metric_alarm" "CostAlloactionTagAlarm" {
   comparison_operator = var.comparison_operator
   evaluation_periods  = var.evaluation_periods
   threshold           = var.threshold
-  statistic           = var.statistic
-  metric_name         = var.metric_name
-  namespace           = var.namespace
-  period              = var.period
+  statistic          = var.statistic
+  metric_name        = var.metric_name
+  namespace          = var.namespace
+  period             = var.period
 }
 
 ########################################################
