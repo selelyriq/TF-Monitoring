eb45b68d9340fca71a6849728f4dcfe8b690127a
lyriq-cloudsolutions <lyriq@geniouscloudsolutions.com>
2025-04-04 15:26:01 -0400

    removed iam role

--- main.tf
+++ main.tf
@@ -31,32 +31,6 @@ resource "aws_cloudwatch_metric_alarm" "CostAlloactionTagAlarm" {
 # EC2 to access CloudWatch Metrics
 ########################################################
 
-resource "aws_iam_role" "ec2_cloudwatch_role" {
-  name = "ec2_cloudwatch_role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ec2_cloudwatch_policy_attachment" {
-  role       = aws_iam_role.ec2_cloudwatch_role.name
-  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
-}
-
-resource "aws_iam_instance_profile" "ec2_cloudwatch_profile" {
-  name = var.instance_profile_name
-  role = aws_iam_role.ec2_cloudwatch_role.name
-}
-
 resource "aws_cloudwatch_log_group" "cost_allocation_tag_log_group" {
   name              = var.log_group_name
   retention_in_days = var.retention_in_days
